dca1f61601d78970265248d41cb817aef90b5897
LeoZheng279 <leozheng279@gmail.com>
2025-01-02 22:04:15 +0800

    1. Modifies getHumanChoice, 2.Implements all functions

--- rps.js
+++ rps.js
@@ -10,14 +10,50 @@ function getcomputerChoice()
 
 function getHumanChoice()
 {
-    do 
-    {let human = Number(prompt("Please input your choice:\n 0:Rock, 1:Paper, 2:Scissors"));}
-    while (isNaN(human) || human>2 ||human<0);
-    switch(human)
+    while(1)
     {
-        case 0: return 0;
-        case 1: return 1;
-        case 2: return 2;
+        let human = (prompt("Please input your choice:(Rock, Paper or Scissors)")).toLowerCase();
+        if (human == 'rock')
+            return 0;
+        else if (human == 'paper')
+            return 1;
+        else if (human == 'scissors')
+            return 2;
     }
 }
 
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound()
+{
+    let human = getHumanChoice();
+    console.log("Your choice: "+human);
+    let computer = getcomputerChoice();
+    console.log("PC's choice: "+computer);
+    if ((human-computer)==1 || (human-computer)==-2)
+    {
+        console.log("You win!");
+        humanScore ++;
+    }
+    else if (human == computer)
+    {
+        console.log("Tie!");
+    }
+    else
+    {
+        console.log("You lose!")
+        computerScore++;
+    }
+}
+
+for (let i = 0; i<5; i++)
+{
+    playRound();
+}
+
+if(humanScore>computerScore)
+    console.log("\nYou won "+humanScore+" rounds. You won this match!");
+else if(humanScore>computerScore)
+    console.log("\nYou won "+humanScore+" rounds. You lost this match!");
+else console.log("\nTie!");

bde35c4532ec0a9058c8c7522b5fc5cfce75cad9
LeoZheng279 <leozheng279@gmail.com>
2025-02-03 11:03:09 +0800

    finished all functions

--- rps.js
+++ rps.js
@@ -15,47 +15,61 @@ function getcomputerChoice()
     else return 2;
 }
 
-// function getHumanChoice()
-// {
-//     while(1)
-//     {
-//         let human = (prompt("Please input your choice:(Rock, Paper or Scissors)")).toLowerCase();
-//         if (human == 'rock')
-//             return 0;
-//         else if (human == 'paper')
-//             return 1;
-//         else if (human == 'scissors')
-//             return 2;
-//     }
-// }
 
 let humanScore = 0;
 let computerScore = 0;
 let human;
+// 事件要创建在外面，否则会在playRound中重复创建
+// 自己创建的事件要自己分配给对应的元素
+const result = document.querySelector(".result");
+const HTMLhumanScore = document.querySelector("#human");
+    HTMLhumanScore.addEventListener("end", () =>{
+            alert("You won this match!");
+            reset();
+    })
+const HTMLcomputerScore = document.querySelector("#computer");
+HTMLcomputerScore.addEventListener("end", () => {
+        alert("You lost this match!");
+        reset();
+    })
+
+function checkEnd(element)
+{
+    if (humanScore >= 5 || computerScore >= 5){
+        const event = new CustomEvent("end");
+        element.dispatchEvent(event); // 向具体元素分配事件
+    }
+}
+
+function reset()
+{
+    humanScore = 0;
+    computerScore = 0;
+    result.innerHTML = '';
+    HTMLcomputerScore.textContent = 0;
+    HTMLhumanScore.textContent = 0;
+}
 
 function playRound()
 {
-    console.log("Your choice: "+Num2Choice(human));
     let computer = getcomputerChoice();
-    console.log("PC's choice: "+Num2Choice(computer));
+    const round = document.createElement("p");
+    round.textContent = `${Num2Choice(human)} vs ${Num2Choice(computer)}`;
+    result.appendChild(round);
+    
     if ((human-computer)==1 || (human-computer)==-2)
     {
-        console.log("You win!");
         humanScore ++;
+        HTMLhumanScore.textContent = humanScore;
+        checkEnd(HTMLhumanScore);
     }
-    else if (human == computer)
-    {
-        console.log("Tie!");
-    }
+    else if (human == computer);
     else
     {
-        console.log("You lose!")
         computerScore++;
+        HTMLcomputerScore.textContent = computerScore;
+        checkEnd(HTMLcomputerScore);
     }
-    if(humanScore===5)
-        console.log("\nYou won "+humanScore+" rounds. You won this match!");
-    else if(computerScore===5)
-        console.log("\nYou won "+humanScore+" rounds. You lost this match!");
 }
 
 
